8e49640a6caf39e06c82b01308494699af2cb4ea
coa205 <coa205@naver.com>
2017-03-06 11:07:59 +0900

    added Tell his World

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello!{
 	public static void main(String[] args){
 		System,out.println("Hello World");
+		system.out.println("Tell your World");
+		System.out.println("Tell her World");
 	}
 }
